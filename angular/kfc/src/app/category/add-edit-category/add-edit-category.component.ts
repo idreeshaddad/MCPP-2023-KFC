ff0e27ede5b868ba84332de335a4269aca692de5
Idrees Haddad <gelnox@live.com>
2023-05-15 18:42:30 +0300

    - Add Edit category: get id from url and set page mode - Added PageMode Enum

--- angular/kfc/src/app/category/add-edit-category/add-edit-category.component.ts
+++ angular/kfc/src/app/category/add-edit-category/add-edit-category.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PageMode } from 'src/app/enum/pageMode.enum';
+import { Category } from 'src/app/models/category.model';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-add-edit-category',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditCategoryComponent implements OnInit {
 
-  constructor() { }
+  categoryId!: number;
+  category!: Category;
+  pageMode: PageMode = PageMode.add;
+
+  pageModeEnum = PageMode;
+
+  constructor(private catSvc: CategoryService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.getCategoryIdFromUrl();
+
+    if (this.pageMode == PageMode.edit) {
+
+      this.loadCategory();
+    }
   }
+
+  //#region Private Functions
+
+  private getCategoryIdFromUrl(): void {
+
+    if (this.activatedRoute.snapshot.paramMap.get('id')) {
+
+      this.categoryId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+      this.pageMode = PageMode.edit;
+    }
+  }
+
+  private loadCategory(): void {
+
+    this.catSvc.getCategory(this.categoryId).subscribe({
+      next: (categoryFromApi: Category) => {
+        this.category = categoryFromApi;
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error(err.error);
+      }
+    });
+  }
+
+  //#endregion
 
 }
