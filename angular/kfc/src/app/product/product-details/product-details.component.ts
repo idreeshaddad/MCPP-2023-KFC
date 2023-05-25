7fa30815955db3d672299638f83dafa861695828
Idrees Haddad <gelnox@live.com>
2023-05-25 18:21:41 +0300

    NG finished product details page

--- angular/kfc/src/app/product/product-details/product-details.component.ts
+++ angular/kfc/src/app/product/product-details/product-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/products/product.model';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private productSvc: ProductService,
+    private activatedRoute: ActivatedRoute) { }
+
+  productId!: number;
+  product?: Product;
 
   ngOnInit(): void {
+
+    this.getProductsIdFromUrl();
+
+    if (this.productId) {
+
+      this.loadProducts();
+    }
   }
+
+  //#region Private Functions
+
+  private getProductsIdFromUrl(): void {
+
+    if (this.activatedRoute.snapshot.paramMap.get('id')) {
+
+      this.productId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    }
+  }
+
+  private loadProducts(): void {
+
+    this.productSvc.getProduct(this.productId).subscribe({
+      next: (productsFromApi: Product) => {
+        this.product = productsFromApi;
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error(err.error);
+      }
+    });
+  }
+
+  //#endregion
+
 
 }
